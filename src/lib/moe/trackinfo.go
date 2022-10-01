d750d07c77c3a74bdf2c9317ec2c9901e74a0c49
Niakr1s <pavel2188@gmail.com>
2022-10-01 10:09:11 +0300

    moved trackinfo to separate file

---
+++ src/lib/moe/trackinfo.go
@@ -0,0 +1,51 @@
+package moe
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	TRACK_UPDATE         = "TRACK_UPDATE"
+	TRACK_UPDATE_REQUEST = "TRACK_UPDATE_REQUEST"
+)
+
+type TrackInfo struct {
+	Op   int `json:"op"`
+	Data struct {
+		Song      Song      `json:"song"`
+		StartTime time.Time `json:"startTime"`
+
+		LastPlayed []Song `json:"lastPlayed"`
+		Listeners  int    `json:"listeners"`
+	} `json:"d"`
+	Type string `json:"t"` // TRACK_UPDATE or TRACK_UPDATE_REQUEST
+}
+
+func (s TrackInfo) String() string {
+	return fmt.Sprintf("Type: %s, StartTime: %v, Song: [%v]", s.Type, s.Data.StartTime, s.Data.Song)
+}
+
+type Song struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Artists []struct {
+		ID         int         `json:"id"`
+		Name       string      `json:"name"`
+		NameRomaji interface{} `json:"nameRomaji"`
+		Image      string      `json:"image"`
+	} `json:"artists"`
+	Duration int `json:"duration"`
+}
+
+func (s Song) String() string {
+	res := fmt.Sprintf("ID: %d, Title: %s, Artist: %s, Duration: %v", s.ID, s.Title, s.Artist(), s.Duration)
+	return res
+}
+
+func (s Song) Artist() string {
+	if len(s.Artists) == 0 {
+		return ""
+	}
+	return s.Artists[0].Name
+}
--- src/lib/moe/ws.go
+++ src/lib/moe/ws.go
@@ -2,7 +2,6 @@ package moe
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/url"
 	"time"
@@ -20,51 +19,6 @@ type wsWelcomeMsg struct {
 		Message   string `json:"message"`
 		Heartbeat int    `json:"heartbeat"`
 	} `json:"d"`
-}
-
-type Song struct {
-	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	Artists []struct {
-		ID         int         `json:"id"`
-		Name       string      `json:"name"`
-		NameRomaji interface{} `json:"nameRomaji"`
-		Image      string      `json:"image"`
-	} `json:"artists"`
-	Duration int `json:"duration"`
-}
-
-func (s Song) String() string {
-	res := fmt.Sprintf("ID: %d, Title: %s, Artist: %s, Duration: %v", s.ID, s.Title, s.Artist(), s.Duration)
-	return res
-}
-
-func (s Song) Artist() string {
-	if len(s.Artists) == 0 {
-		return ""
-	}
-	return s.Artists[0].Name
-}
-
-const (
-	TRACK_UPDATE         = "TRACK_UPDATE"
-	TRACK_UPDATE_REQUEST = "TRACK_UPDATE_REQUEST"
-)
-
-type TrackInfo struct {
-	Op   int `json:"op"`
-	Data struct {
-		Song      Song      `json:"song"`
-		StartTime time.Time `json:"startTime"`
-
-		LastPlayed []Song `json:"lastPlayed"`
-		Listeners  int    `json:"listeners"`
-	} `json:"d"`
-	Type string `json:"t"` // TRACK_UPDATE or TRACK_UPDATE_REQUEST
-}
-
-func (s TrackInfo) String() string {
-	return fmt.Sprintf("Type: %s, StartTime: %v, Song: [%v]", s.Type, s.Data.StartTime, s.Data.Song)
 }
 
 type MoeWs struct {
