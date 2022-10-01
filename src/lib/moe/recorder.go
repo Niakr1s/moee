6b1ec21cd90d668ad01a0609f59457dfcb53c9b3
Niakr1s <pavel2188@gmail.com>
2022-10-01 09:43:53 +0300

    ws/recorder initial

---
+++ src/lib/moe/recorder.go
@@ -0,0 +1,44 @@
+package moe
+
+import (
+	"log"
+
+	"github.com/niakr1s/moee/src/lib/recorder"
+)
+
+type Recorder struct {
+	rec *recorder.Recorder
+	ws  *MoeWs
+}
+
+func NewRecorder() *Recorder {
+	return &Recorder{
+		rec: recorder.NewRecorder("https://listen.moe/stream"),
+		ws:  &MoeWs{},
+	}
+}
+
+func (rec *Recorder) Start() error {
+	err := rec.ws.Connect()
+	if err != nil {
+		return err
+	}
+
+	err = rec.rec.StartRecord()
+	if err != nil {
+		return err
+	}
+
+	trackInfoCh := rec.ws.wsTrackCh
+	trackCh := rec.rec.TrackCh()
+
+	for {
+		select {
+		case info := <-trackInfoCh:
+			log.Printf("info: %v", info)
+		case track := <-trackCh:
+			log.Printf("track: %v", track)
+			// in wsTrack we have already info about new song, so let's copy song's info about
+		}
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/niakr1s/moee/src/lib/moe"
+)
+
+func main() {
+	rec := moe.NewRecorder()
+	if err := rec.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
