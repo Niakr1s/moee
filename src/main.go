65b7370e86238ab95f32f73a8a9de3d748cb78b7
Niakr1s <pavel2188@gmail.com>
2022-10-01 11:34:54 +0300

    finished executable

--- src/main.go
+++ src/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/niakr1s/moee/src/lib/moe"
+	"github.com/niakr1s/moee/src/lib/util"
 )
 
 func main() {
-	rec := moe.NewRecorder("d:/tmp")
-	rec.DiscardFirstTrack = true
-	rec.SaveOnlyWithLyrics = true
+	if len(os.Args) != 2 {
+		log.Fatalf("USAGE: moee [savedir]")
+	}
+
+	savedir := os.Args[1]
+	savedir, err := util.ToAbs(savedir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.MkdirAll(savedir, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rec := moe.NewRecorder(savedir)
 	if err := rec.Start(); err != nil {
 		log.Fatal(err)
 	}
